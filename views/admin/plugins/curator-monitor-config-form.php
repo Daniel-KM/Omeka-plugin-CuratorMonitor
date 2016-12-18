e6fae5b8e2fdbbd71e8389aa33fa23711d89f172
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-12-19 00:00:00 +0100

    Added a class in config form.

--- views/admin/plugins/curator-monitor-config-form.php
+++ views/admin/plugins/curator-monitor-config-form.php
@@ -8,7 +8,7 @@
 </style>
 <fieldset id="fieldset-curator-monitor-elements"><legend><?php echo __('Elements'); ?></legend>
     <?php $monitorElementSet = $this->monitor()->getElementSet(); ?>
-    <p><?php echo __('To manage elements (repeatable or not, steppable or not, with list of terms or not...), go to %sSettings%s, then %sElement Sets%s, then %sMonitor%s.',
+    <p class="explanation"><?php echo __('To manage elements (repeatable or not, steppable or not, with list of terms or not...), go to %sSettings%s, then %sElement Sets%s, then %sMonitor%s.',
             '<a href="' . url('settings') . '">', '</a>',
             '<a href="' . url('element-sets') . '">', '</a>',
             '<a href="' . url('element-sets/edit/' . $monitorElementSet->id) . '">', '</a>'); ?></p>
